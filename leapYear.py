4bf30646378e4870618b171577cf96711ef7e075
bbk-Dankwardt <24-hbfi1.dankwardt@bbk.schule>
2025-05-05 11:58:16 +0200

    heheheheheha

--- leapYear.py
+++ leapYear.py
@@ -1,10 +1,22 @@
 
 def isLeapYear(year):
     # Hier müssen Sie erweitern ...
-    return False
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 
 def main():
-    isLeapYear(2025)
-
+    year = int(input("Welches Jahr soll überprüft werden? "))
+    if isLeapYear(year):
+        print( year, "ist ein Schaltjahr" )
+    else:
+        print( year, "ist KEIN Schaltjahr" )
 if __name__ == "__main__":
     main()
